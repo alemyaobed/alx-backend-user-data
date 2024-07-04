3cb063a714b275247bf3a00480d9701cc54a5d46
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-04 14:47:14 +0000

    docstrings

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -41,7 +41,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        ''''''
+        ''' The constructor method for the class'''
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
