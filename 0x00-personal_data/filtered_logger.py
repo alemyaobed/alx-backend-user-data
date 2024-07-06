f8bc67d18b5660893575f91c1750fce6344d41ce
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-06 09:36:20 +0000

    the implementation of task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,6 +16,8 @@ from typing import List
 import re
 import logging
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
 
 def filter_datum(
         fields: List[str], redaction: str,
@@ -29,6 +31,41 @@ def filter_datum(
         # Substitute the pattern in the message
         message = re.sub(pattern, replacement, message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    '''
+    Use user_data.csv for this task
+
+    Implement a get_logger function that takes no arguments and returns a
+    logging.Logger object.
+
+    The logger should be named "user_data" and only log up to logging.INFO
+    level. It should not propagate messages to other loggers. It should have
+    a StreamHandler with RedactingFormatter as formatter.
+
+    Create a tuple PII_FIELDS constant at the root of the module containing the
+    fields from user_data.csv that are considered PII. PII_FIELDS can contain
+    only 5 fields - choose the right list of fields that can are considered as
+    “important” PIIs or information that you must hide in your logs. Use it to
+    parameterize the formatter.
+
+    Tips:
+
+    What Is PII, non-PII, and personal data?
+    Uncovering Password Habits
+    '''
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
