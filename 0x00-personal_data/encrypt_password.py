0bed4b54af7cfab045aa15e409023d3d5da1a208
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-06 12:02:41 +0000

    some major changes

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-'''
-User passwords should NEVER be stored in plain text in a database.
-
-Implement a hash_password function that expects one string argument
-name password and returns a salted, hashed password, which is a byte
-string.
-
-Use the bcrypt package to perform the hashing (with hashpw).
-'''
-from bcrypt import hashpw, gensalt
+"""Encrypting pswds with bcrypt."""
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    ''' Hashing using bcrypt and returning a byte string'''
-    hashed = hashpw(password.encode('utf-8'), gensalt())
-    return hashed
+    """
+    Takes in string arg, converts to unicode
+    Returns salted, hashed pswd as bytestring
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
