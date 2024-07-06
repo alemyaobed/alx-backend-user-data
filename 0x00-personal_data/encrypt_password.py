fc523336718dbe15ced4661e699b7171a89b38c6
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-06 12:23:13 +0000

    some changes

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 '''
-User passwords should NEVER be stored in plain text in a database.
-
-Implement a hash_password function that expects one string argument
-name password and returns a salted, hashed password, which is a byte
-string.
-
-Use the bcrypt package to perform the hashing (with hashpw).
+Hashing passwords
 '''
 import bcrypt
 
@@ -18,15 +12,5 @@ def hash_password(password: str) -> bytes:
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    '''
-    Implement an is_valid function that expects 2 arguments and returns a
-    boolean.
-
-    Arguments:
-
-    hashed_password: bytes type
-    password: string type
-    Use bcrypt to validate that the provided password matches the hashed
-    password.
-    '''
+    ''' Checks if a hashed password is valid'''
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
