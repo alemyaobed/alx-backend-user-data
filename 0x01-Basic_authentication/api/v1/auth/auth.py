001402ff780c0266d8400444a915e65b807025e8
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-09 15:50:36 +0000

    implementation of the require auth for paths to determine whether a path needs authentication or not

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,8 +10,16 @@ class Auth:
     The class for handling authentication
     '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        ''' Returns False for now '''
-        return False
+        ''' Determines if a given path requires authentication '''
+        if path and excluded_paths:
+
+            # Normalize the path to ensure it ends with a slash
+            if not path.endswith('/'):
+                path += '/'
+
+            if path in excluded_paths:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         ''' Returns None for now '''
