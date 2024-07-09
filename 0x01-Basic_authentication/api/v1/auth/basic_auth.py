8f4decb689197849638455d38c4725a8d7404064
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-09 19:06:50 +0000

    addition of basic auth class and switch based on the auth type environment variable

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module for BasicAuth that inherits from Auth
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    '''
+    The class for handling  basic authentication
+    '''
