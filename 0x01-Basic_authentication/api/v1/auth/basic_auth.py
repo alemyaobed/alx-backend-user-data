b8e2e6720c84773defefb5971c3babe0a8abaf1e
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-09 22:16:59 +0000

    implementation of credentials extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,14 @@ class BasicAuth(Auth):
     '''
     The class for handling  basic authentication
     '''
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        '''
+        Returns the Base64 part of the Authorization header for a
+        Basic Authentication
+        '''
+        if authorization_header is None or not isinstance(authorization_header, str) or not (':' in authorization_header):
+            return None, None
+        username, password = tuple(authorization_header.split(':'))
+        
+        return username, password
