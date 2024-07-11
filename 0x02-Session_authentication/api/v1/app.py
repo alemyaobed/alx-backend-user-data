365643e08e8cd88a02fddefa627bd629f1cb57dc
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-11 10:16:04 +0000

    some minor changes

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,10 +38,7 @@ def before_request():
                  '/api/v1/forbidden/', '/api/v1/auth_session/login/']
 
     if auth and auth.require_auth(request.path, path_list):
-        if not auth.authorization_header(request):
-            abort(401)
-        if not (auth.authorization_header(request) and
-                auth.session_cookie(request)):
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
             abort(401)
         if not auth.current_user(request):
             abort(403)
