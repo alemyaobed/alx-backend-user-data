d32998fa6be05b36b7a6802cd9981132fe6ed016
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-16 17:08:04 +0000

    set the echo to fals

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -15,7 +15,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,7 +31,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         ''' Adds a user object to the database '''
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+        return new_user
