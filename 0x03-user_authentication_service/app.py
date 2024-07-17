5112b2956d06187bd3b894fe395345a456cd38cf
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-17 17:35:13 +0000

    the implementation of the end point to register new users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 '''
 The Flask App model
 '''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
@@ -12,6 +14,19 @@ def home():
     ''' The home route that returns a JSON payload. '''
     payload = {"message": "Bienvenue"}
     return jsonify(payload)
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    ''' An end-point to register a new user '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email=email, password=password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    else:
+        return jsonify({"email": f"{email}", "message": "user created"})
 
 
 if __name__ == '__main__':
