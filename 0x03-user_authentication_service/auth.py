5b180da17491117b2b4983de7acc875db89088c6
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-18 11:22:52 +0000

    the implementation of the method for the auth class to destroy a session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,3 +80,14 @@ class Auth:
             except NoResultFound:
                 pass
         return None
+
+    def destroy_session(self, user_id: int) -> None:
+        '''
+        Updates the corresponding user’s session ID to None hence destroying
+        the session'''
+        if user_id:
+            try:
+                self._db.update_user(user_id=user_id, session_id=None)
+            except ValueError:
+                pass
+        return None
